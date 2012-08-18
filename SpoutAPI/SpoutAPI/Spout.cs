862269dd76432159522b41d17c34a2f230dbce69
Dalton Pelc <daltonjpelc@gmail.com>
2012-08-18 18:04:49 -0400

    Added Spout class and Engine/Client/Server interfaces
    Signed-off-by: Dalton Pelc <daltonjpelc@gmail.com>

--- SpoutAPI/SpoutAPI/Spout.cs
+++ SpoutAPI/SpoutAPI/Spout.cs
@@ -5,7 +5,24 @@ using System.Text;
 
 namespace SpoutAPI
 {
-    public class Spout
+    public sealed static class Spout
     {
+        private static Engine instance;
+        public static Engine Engine
+        {
+            get { return instance; }
+            set
+            {
+                if (instance == null)
+                    instance = value;
+                else
+                    throw new InvalidOperationException("Cannot set the Engine a second time");
+            }
+        }
+
+        public static void Stop()
+        {
+            Engine.Stop();
+        }
     }
 }
