7c1f8f06ee2c532678cb0d962e113b072a11b5c9
Dalton Pelc <daltonjpelc@gmail.com>
2012-08-18 18:33:13 -0400

    Quick comment test

--- SpoutAPI/SpoutAPI/Server.cs
+++ SpoutAPI/SpoutAPI/Server.cs
@@ -7,6 +7,9 @@ namespace SpoutAPI
 {
     public interface Server : Engine
     {
+        /// <summary>
+        /// If the server has whitelisting turned on or off.
+        /// </summary>
         bool Whitelist
         {
             get;
