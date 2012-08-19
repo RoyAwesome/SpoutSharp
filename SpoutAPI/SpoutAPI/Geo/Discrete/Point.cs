7a138044f924335282f94a00d3c103bb61b3ecad
Chris Sanders <zidane@outlook.com>
2012-08-18 22:42:54 -0400

    Some more operator overloading fun. Signed-off-by: Chris Sanders <zidane@outlook.com>

--- SpoutAPI/SpoutAPI/Geo/Discrete/Point.cs
+++ SpoutAPI/SpoutAPI/Geo/Discrete/Point.cs
@@ -7,9 +7,10 @@ using System.Text;
 
 namespace SpoutAPI.Geo.Cuboid
 {
-    public class Point: Vector3
+    public class Point
     {
         private readonly static long serialVersionUID = 1L;
+        internal readonly Vector3 vector;
         internal readonly World world;
         //Hashcode
         [NonSerialized]
@@ -17,19 +18,78 @@ namespace SpoutAPI.Geo.Cuboid
         [NonSerialized]
         private volatile int hashcode = 0;
 
-        public Point(World world, Vector3 vector) : base(vector)
+        public Point(World world, Vector3 vector)
         {
             this.world = world;
+            this.vector = vector;
         }
 
-        public Point(World world, float x, float y, float z) : base(x, y, z)
+        public Point(World world, float x, float y, float z)
         {
             this.world = world;
+            vector = new Vector3(x, y, z);
         }
 
-        public Point operator/(int val) 
+        public static bool operator ==(Point l, Point r)
         {
-            Vector3 toDivide = new Vector3(val, val, val);
-            
-    }
+            if (l.world != r.world)
+            {
+                return false;
+            }
+            if (l.vector != r.vector)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool operator !=(Point l, Point r)
+        {
+            if (l.world == r.world)
+            {
+                return false;
+            }
+            if (l.vector == r.vector)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static Point operator +(Point l, Point r)
+        {
+            if (l.world != r.world)
+            {
+                throw new InvalidOperationException("Cannot add two points with mismatch worlds together");
+            }
+            return new Point(l.world, Vector3.Add(l.vector, r.vector));
+        }
+
+        public static Point operator -(Point l, Point r)
+        {
+            if (l.world != r.world)
+            {
+                throw new InvalidOperationException("Cannot add two points with mismatch worlds together");
+            }
+            return new Point(l.world, Vector3.Subtract(l.vector, r.vector));
+        }
+
+        public static Point operator /(Point l, Point r)
+        {
+            if (l.world != r.world)
+            {
+                throw new InvalidOperationException("Cannot add two points with mismatch worlds together");
+            }
+            return new Point(l.world, Vector3.Divide(l.vector, r.vector));
+        }
+
+        public static Point operator *(Point l, Point r)
+        {
+            if (l.world != r.world)
+            {
+                throw new InvalidOperationException("Cannot add two points with mismatch worlds together");
+            }
+            return new Point(l.world, Vector3.Multiply(l.vector, r.vector));
+        }
+    }  
 }
